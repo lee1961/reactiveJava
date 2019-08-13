05c01308abf402c76e7c82bf7ddf56c6d8516dae
victor lee juan liang <lee1961@purdue.edu>
2019-08-13 17:45:55 -0400

    installed gradle onto it

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import io.reactivex.Observable;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String [] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+
+
+        Observable<Integer> ob = Observable.just(5);
+        
+    }
+}
